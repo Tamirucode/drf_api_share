0b2751129f3dfcfbace9779c743b3f43e6b383f2
Tamirucode <tam.egu76@gmail.com>
2023-10-07 14:59:07 +0000

    added refactor todolist and detail views using generics

--- todolists/views.py
+++ todolists/views.py
@@ -1,77 +1,26 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
+from drf_api_share.permissions import IsOwnerOrReadOnly
 from .models import ToDoList
 from .serializers import ToDoListSerializer
-from drf_api_share.permissions import IsOwnerOrReadOnly
 
 
-class ToDoListList(APIView):
+class ToDoListList(generics.ListCreateAPIView):
     """
-    List todolist or create a todolist if logged in
+    List todolist or create a post if logged in
+    The perform_create method associates the todolist with the logged in user.
     """
     serializer_class = ToDoListSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
-    def get(self, request):
-        todolists = ToDoList.objects.all()
-        serializer = ToDoListSerializer(todolists, many= True)
-        return Response(serializer.data)
-    
-        
-    def post(self, request):
-        serializer = ToDoListSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-      
-        )
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = ToDoList.objects.all()
 
-class ToDoListDetail(APIView):
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ToDoListDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve a todolist and edit or delete it if you own it
+    Retrieve a todolist and edit or delete it if you own it.
     """
+    serializer_class = ToDoListSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = ToDoListSerializer
-
-    def get_object(self, pk):
-        try:
-            todolist = ToDoList.objects.get(pk=pk)
-            self.check_object_permissions(self.request, todolist)
-            return todolist
-        except ToDoList.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        todolist = self.get_object(pk)
-        serializer = ToDoListSerializer(
-            todolist, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        todolist = self.get_object(pk)
-        serializer = ToDoListSerializer(
-            todolist, data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-                )
-
-    def delete(self, request, pk):
-        todolist = self.get_object(pk)
-        todolist.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = ToDoList.objects.all()
